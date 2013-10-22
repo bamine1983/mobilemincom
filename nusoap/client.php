71931d930bd826088e438f940fe824b575022667
Amine <amine.bouchama@gmail.com>
2013-10-22 23:21:56 +0100

    Initialisation

--- nusoap/client.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('lib/nusoap.php');
-$serverpath ='http://www.nusoap.dev/serveur.php';
-$param = array('prenom'=>'Amine');
-$client = new nusoap_client($serverpath);
-$result = $client->call('bonjour', $param);
-print($client->request);
-?>
